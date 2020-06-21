c8d73428c94090bbad381e796090f8ef0056523b
unknown <kuba_k7@onet.pl>
2020-06-21 15:33:37 +0200

    Added form for editing smoothies

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,20 +1,75 @@
 <template>
-    <div class="edit-smoothie container">
-      <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+      <h2>Edit {{ smoothie.title }} Smoothie</h2>
+      <form @submit.prevent="EditSmoothie">
+        <div class="field title">
+          <label for="title">Smoothie Title</label>
+          <input type="text" name="title" v-model="smoothie.title">
+        </div>
+        <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+          <label for="ingredient">Ingredient:</label>
+          <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+          <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+        </div>
+        <div class="field add-ingredient">
+          <label for="add-ingredient">Add an ingredient:</label>
+          <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+        </div>
+        <div class="field center-align">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn pink">Update Smoothie</button>
+        </div>
+      </form>
     </div>
 </template>
 
 <script>
+    import db from '@/firebase/init'
+
     export default {
         name: "EditSmoothie",
         data(){
           return{
-
+            smoothie: null,
+            another: null,
+            feedback: null
           }
+        },
+        created(){
+          let ref = db.collection('smoothies').where(`slug`, '==', this.$route.params.smoothie_slug)
+          ref.get().then(snapshot =>
+          snapshot.forEach(doc => {
+            this.smoothie = doc.data()
+            this.smoothie.id = doc.id
+          }))
         }
     }
 </script>
 
-<style scoped>
+<style>
+
+  .edit-smoothie{
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+  }
+  .edit-smoothie h2{
+    font-size: 2em;
+    margin: 20px auto;
+  }
+
+  .edit-smoothie .field{
+    margin: 20px auto;
+    position: relative;
+  }
+
+  .edit-smoothie .delete{
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
 
 </style>
